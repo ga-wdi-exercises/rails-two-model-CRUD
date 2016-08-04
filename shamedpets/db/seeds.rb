33efbed0a48c8f3a374bb8c2fb899bd97ca2d660
Daniela Noir <daniela@danielanoir.com>
2016-08-04 19:07:55 -0400

    finished.

--- shamedpets/db/seeds.rb
+++ shamedpets/db/seeds.rb
@@ -3,4 +3,5 @@ Dog.destroy_all
 
 Felony = Offense.create(crime: "Felony", picture: "https://s-media-cache-ak0.pinimg.com/736x/cd/1a/ac/cd1aac88a5413424529c648c0b289c73.jpg", description: "A crime, typically involving violence.")
 Misdemeanor = Offense.create(crime: "Misdemeanor", picture: "http://cdn.ebaumsworld.com/mediaFiles/picture/2235368/84069398.jpg", description: "A minor wrongdoing.")
+Harassment = Offense.create(crime: "Harassment", picture: "http://caimages.co.uk/listing_images/F97X449_BL.jpg", description: "Bothering people, dogs, squirels... basically dog-jerks.")
 Gross = Offense.create(crime: "Gross", picture: "http://cdn.ebaumsworld.com/mediaFiles/picture/2235368/84069396.jpg", description: "Anything offensively gross that should not be allowed ever.")
