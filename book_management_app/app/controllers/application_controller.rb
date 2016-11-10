82d100260a5fff5db5a53788f891d8458fd57d2f
Angel Valant <angelgithub@gmail.com>
2016-11-10 00:10:00 -0500

    adds error handling

--- book_management_app/app/controllers/application_controller.rb
+++ book_management_app/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :couldnt_find_record
+
+  private
+  def couldnt_find_record
+    redirect_to root_url, notice: "That record doesn't exist."
+  end
+
+  def no_method_error
+    redirect_to root_url, notice: "This error has been reported to the developer."
+  end
 end
